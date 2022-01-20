abe30dc75a2dc86670d4161e04703839848cdff1
David A <ddvlzsz@gmail.com>
2022-01-20 22:43:32 +0100

    wip lsp experiments

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,7 +2,6 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
